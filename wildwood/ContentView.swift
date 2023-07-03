1686b09c54519f7a3aa8f57ae96a66c7cbe547c3
kritbovorn <kritbovorn@gmail.com>
2023-07-03 13:50:41 +0700

    Initial Commit

---
+++ wildwood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wildwood
+//
+//  Created by krit on 3/7/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wildwood/wildwoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  wildwoodApp.swift
+//  wildwood
+//
+//  Created by krit on 3/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct wildwoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
